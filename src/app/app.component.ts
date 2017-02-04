fb609688cdec191cb7a209e5bab7d90ecef3c4a3
John jaeckle <daddyschmack@gmail.com>
2017-02-04 12:46:17 -0600

    end of tutorial 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import {TodoDataService} from "./todo-data.service";
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
+
 export class AppComponent {
-  title = 'app works!';
+  // Ask Angular DI system to inject the dependency
+  // associated with the dependency injection token `TodoDataService`
+  // and assign it to a property called `todoDataService`
+
+  newTodo: Todo = new Todo();
+
+  constructor(private todoDataService: TodoDataService){
+
+  }
+ //service is now available as this .todoDataService
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo){
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo){
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos(){
+    return this.todoDataService.getAllTodos();
+  }
+
 }
